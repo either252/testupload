45b3dfaf936cf92d61bd7267ab60975157225058
either252 <either@naver.com>
2020-11-19 12:11:49 +0900

    ex003 commit

---
+++ src/main/java/org/zerock/domain/AuthVO.java
@@ -0,0 +1,6 @@
+package org.zerock.domain;
+
+public class AuthVO {
+	private String username;
+	private String authority;
+}
---
+++ src/main/java/org/zerock/domain/memberVO.java
@@ -0,0 +1,7 @@
+package org.zerock.domain;
+
+public class memberVO {
+	private String username;
+	private String password;
+	private char enabled;
+}
